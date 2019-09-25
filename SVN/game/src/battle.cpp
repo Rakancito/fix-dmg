b7e3424a050d16ae6f84ee89c05b24441397d762
Robert <55710848+Rakancito@users.noreply.github.com>
2019-09-25 11:17:03 -0500

    Update battle.cpp

--- SVN/game/src/battle.cpp
+++ SVN/game/src/battle.cpp
@@ -24,11 +24,10 @@ int battle_hit(LPCHARACTER pkAttacker, LPCHARACTER pkVictim, int & iRetDam)
 // Add after
 	if(pkAttacker->IsPC())
 	{
-
 		const bool bAttacking = (get_dword_time() - pkAttacker->GetLastAttackTime()) < pkAttacker->IsRiding() ? 800 : 700;
 		if (!bAttacking)
 			return BATTLE_NONE;
-		if (test_server&&ch->IsPC())
+		if (test_server&&pkAttacker->IsPC())
 			pkAttacker->ChatPacket(CHAT_TYPE_INFO, "Melee Attack: %d", get_dword_time() - ch->GetLastAttackTime());
 		if (!battle_distance_valid(pkAttacker, pkVictim))
 			return BATTLE_NONE;
