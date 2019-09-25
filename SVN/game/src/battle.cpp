fde5dc3823f3f731003e376812613fb344ad02dc
Robert <55710848+Rakancito@users.noreply.github.com>
2019-09-25 11:25:00 -0500

    Update battle.cpp

--- SVN/game/src/battle.cpp
+++ SVN/game/src/battle.cpp
@@ -10,8 +10,8 @@ int battle_melee_attack(LPCHARACTER ch, LPCHARACTER victim)
 		const bool bAttacking = (get_dword_time() - ch->GetLastAttackTime()) < ch->IsRiding() ? 800 : 700;
 		if (!bAttacking)
 			return BATTLE_NONE;
-		if (test_server&&ch->IsPC())
-			ch->ChatPacket(CHAT_TYPE_INFO, "Melee Attack: %d", get_dword_time() - ch->GetLastAttackTime());
+		//if (test_server&&ch->IsPC())
+		//	ch->ChatPacket(CHAT_TYPE_INFO, "Melee Attack: %d", get_dword_time() - ch->GetLastAttackTime());
 		if (!battle_distance_valid(ch, victim))
 			return BATTLE_NONE;
 	}
@@ -27,8 +27,8 @@ int battle_hit(LPCHARACTER pkAttacker, LPCHARACTER pkVictim, int & iRetDam)
 		const bool bAttacking = (get_dword_time() - pkAttacker->GetLastAttackTime()) < pkAttacker->IsRiding() ? 800 : 700;
 		if (!bAttacking)
 			return BATTLE_NONE;
-		if (test_server&&pkAttacker->IsPC())
-			pkAttacker->ChatPacket(CHAT_TYPE_INFO, "Melee Attack: %d", get_dword_time() - ch->GetLastAttackTime());
+		//if (test_server&&pkAttacker->IsPC())
+		//	pkAttacker->ChatPacket(CHAT_TYPE_INFO, "Melee Attack: %d", get_dword_time() - ch->GetLastAttackTime());
 		if (!battle_distance_valid(pkAttacker, pkVictim))
 			return BATTLE_NONE;
 	}
